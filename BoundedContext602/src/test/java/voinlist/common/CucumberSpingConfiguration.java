86341b9fa1e144e7ec2f006bfa8975a2641e4e8e
kibum0405 <top5w5@naver.com>
2023-10-25 06:45:23 +0000

    불필요 파일 제거

--- BoundedContext602/src/test/java/voinlist/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package voinlist.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import voinlist.BoundedContext602Application;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { BoundedContext602Application.class })
-public class CucumberSpingConfiguration {}
